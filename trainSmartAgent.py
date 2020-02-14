418362cd87918e5b4de47e993f2f4bcf6ee880ee
AnwarHaroon <anwarharoon@AnwarHaroonsMBP.home>
2020-02-14 13:09:13 -0500

    longer run

--- trainSmartAgent.py
+++ trainSmartAgent.py
@@ -531,7 +531,7 @@ netParams.connParams['IV4->IIT'] = {
 #Simulation options
 simConfig = specs.SimConfig()           # object of class SimConfig to store simulation configuration
 
-simConfig.duration = 1000 # 0.1e5                      # Duration of the simulation, in ms
+simConfig.duration = 50000 # 0.1e5                      # Duration of the simulation, in ms
 simConfig.dt = 0.2                            # Internal integration timestep to use
 simConfig.verbose = False                       # Show detailed messages
 simConfig.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict with traces to record
