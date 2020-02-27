602ddfcb4d8f58a7445b42ef0c6a2b04b891229a
Sam Neymotin <samnemo@gmail.com>
2020-02-27 11:24:55 -0500

    get rid of most of the hard-coded indices

--- plotWeights.py
+++ plotWeights.py
@@ -1,37 +1,43 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+import pandas as pd
+simConfig = pickle.load(open('data/simConfig.pkl','rb'))
 
-A = numpy.loadtxt('data/AdjustableWeights.txt')
+dstartidx = {p:simConfig['net']['pops'][p]['cellGids'][0] for p in simConfig['net']['pops'].keys()} # starting indices for each population
+dendidx = {p:simConfig['net']['pops'][p]['cellGids'][-1] for p in simConfig['net']['pops'].keys()} # ending indices for each population
+
+A = np.loadtxt('data/AdjustableWeights.txt')
 postN = A[:,2]
-postN_MR = numpy.where(postN>1183)
-postN_ML = numpy.where(postN<1184)#first one is bad
-Mneurons_R = numpy.unique(postN[postN_MR])
-Mneurons_L = numpy.unique(postN[postN_ML])
+postN_MR = np.where(postN>=dstartidx['MR'])
+postN_ML = np.where(postN<dstartidx['MR'])#first one is bad
+Mneurons_R = np.unique(postN[postN_MR])
+Mneurons_L = np.unique(postN[postN_ML])
 
 times = A[:,0]
-utimes = numpy.unique(times)
+utimes = np.unique(times)
 
-#numpy.zeros(shape=(20,20))
+#np.zeros(shape=(20,20))
 All_ML_weights = []
 All_MR_weights = []
 for t in utimes:
-    ctime_inds = numpy.where(times==t)
+    ctime_inds = np.where(times==t)
     ctime_postNs = A[ctime_inds,2]
     ctime_preNs = A[ctime_inds,1]
     ctime_weights = A[ctime_inds,4]
     ML_weights = []
     MR_weights = []
     for n in ctime_postNs[0]:
-        cn_preNs = ctime_preNs[0][numpy.where(ctime_postNs[0]==n)]
-        if len(cn_preNs)>len(numpy.unique(cn_preNs)):
-            print(len(cn_preNs)-len(numpy.unique(cn_preNs)))
-        cn_weights = ctime_weights[0][numpy.where(ctime_postNs[0]==n)]       
+        cn_preNs = ctime_preNs[0][np.where(ctime_postNs[0]==n)]
+        if len(cn_preNs)>len(np.unique(cn_preNs)):
+            print(len(cn_preNs)-len(np.unique(cn_preNs)))
+        cn_weights = ctime_weights[0][np.where(ctime_postNs[0]==n)]       
         if n in Mneurons_R:
-            MR_weights.append(numpy.sum(cn_weights))
+            MR_weights.append(np.sum(cn_weights))
         elif n in Mneurons_L:
-            ML_weights.append(numpy.sum(cn_weights))
-    All_ML_weights.append(numpy.sum(ML_weights))
-    All_MR_weights.append(numpy.sum(MR_weights))
+            ML_weights.append(np.sum(cn_weights))
+    All_ML_weights.append(np.sum(ML_weights))
+    All_MR_weights.append(np.sum(MR_weights))
 
 #plt.subplot(1,2,1)
 plt.plot(utimes,All_ML_weights,'r-')
