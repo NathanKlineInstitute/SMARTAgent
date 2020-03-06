483d1df2627c4fc9aa3abbbdcc8edf0b5f673343
Sam Neymotin <samnemo@gmail.com>
2020-03-06 17:17:12 -0500

    fixup multistep

--- multistepSim.py
+++ multistepSim.py
@@ -5,8 +5,9 @@ import sys
 
 if __name__ == '__main__':
   basefn = sys.argv[1]
-  nstep = int(sys.argv[2])
-  outf = sys.argv[3]
+  ncore = int(sys.argv[2])
+  nstep = int(sys.argv[3])
+  outf = sys.argv[4]
   print(basefn,nstep)
   fpout = open(outf,'w')
   for i in range(nstep):
@@ -16,7 +17,7 @@ if __name__ == '__main__':
     if i > 0:
       d['simtype']['ResumeSim'] = 1
       d['simtype']['ResumeSimFromFile'] = 'data/' + simstr + '_step_' + str(i-1) + '_simConfig.pkl'
-    fnjson = d['sim']['name'] = d['sim']['name'] + '.json'
-    fpout.writelines('myrun 16 ' + fnjson + '\n')
+    fnjson = d['sim']['name'] + '.json'
+    fpout.writelines('myrun ' + str(ncore) + ' ' + fnjson + '\n')
     json.dump(d, open(fnjson,'w'))
   fpout.close()
