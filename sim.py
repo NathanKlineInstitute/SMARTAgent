0de15670351936e9fc4ccb19211e6f77771c5047
AnwarHaroon <anwarharoon@AnwarHaroonsMBP.home>
2020-03-20 13:52:47 -0400

    test saving weights when multiple synapses

--- sim.py
+++ sim.py
@@ -676,6 +676,7 @@ def recordAdjustableWeightsPop (sim, t, popname):
     for cell in lcell:
         for conn in cell.conns:
             if 'hSTDP' in conn:
+                print(conn.preGid, cell.gid, conn.synMech) #testing weight saving
                 sim.simData['synweights'][sim.rank].append([t,conn.plast.params.RLon,conn.preGid,cell.gid,float(conn['hObj'].weight[0])])
     return len(lcell)
                     
