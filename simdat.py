c2322c0bf084721d02ed6e6cc386a72c0d967d41
Sam Neymotin <samnemo@gmail.com>
2020-04-14 16:35:30 -0400

    speeding up movie for syn weights in simdat.py

--- simdat.py
+++ simdat.py
@@ -6,6 +6,7 @@ from conf import dconf
 import os
 import sys
 import anim
+from matplotlib import animation
 
 ion()
 
@@ -35,99 +36,218 @@ def loadsimdat (name=None):
 
 #
 def plotSynWeightsPerTimeStep(pdf,pauset=1, gifpath=None, mp4path=None, framerate=5):
-    utimes = np.unique(pdf.time)
-    maxNMDAwt = np.max(pdf[pdf.syntype=='NMDA']).weight
-    maxAMPAwt = np.max(pdf[pdf.syntype=='AMPA']).weight
-    maxwt = maxNMDAwt+maxAMPAwt
-    minNMDAwt = np.min(pdf[pdf.syntype=='NMDA']).weight
-    minAMPAwt = np.min(pdf[pdf.syntype=='AMPA']).weight
-    minwt = minNMDAwt+minAMPAwt
-    wtrange = 0.1*(maxwt-minwt)
-    fig = plt.figure(figsize=(18,10))
-    gs = fig.add_gridspec(4,8)
-    f_ax1 = fig.add_subplot(gs[0,0:3])
-    f_ax2 = fig.add_subplot(gs[0,4:7])
-    pdfsL = pdf[(pdf.postid>=dstartidx['EML']) & (pdf.postid<=dendidx['EML'])]
-    pdfsR = pdf[(pdf.postid>=dstartidx['EMR']) & (pdf.postid<=dendidx['EMR'])]
-    Lwts = [] #wts of connections onto EML
-    Rwts = [] #wts of connections onto EMR
-    for t in utimes:
-        ct_Lwts = pdfsL[(pdfsL.time==t)].weight
-        ct_Rwts = pdfsR[(pdfsR.time==t)].weight
-        Lwts.append(np.mean(ct_Lwts))
-        Rwts.append(np.mean(ct_Rwts))
-    actionvsproposed = actreward.action-actreward.proposed
-    followtheball = actreward[actionvsproposed==0]
-    f_ax1.plot(actreward.time,actreward.reward,'ko',markersize=2)
-    #f_ax1.plot(actreward.time,actreward.hit,'go',markersize=4) #mark times when the racket hits the ball
-    #f_ax1.plot(followtheball.time,followtheball.action-followtheball.proposed,'ko',markersize=4) #mark times when the racket follows the ball
-    #if dconf['rewardcodes']['scorePoint']==1:
-    #    scoretpnts = actreward.time[actreward.reward>dconf['rewardcodes']['scorePoint']]
-    #    f_ax1.plot(scoretpnts,2*np.ones(shape=(len(scoretpnts),1)),'bo',markersize=4)  
-    f_ax1.set_xlim((0,simConfig['simConfig']['duration']))
-    f_ax1.set_ylim((np.min(actreward.reward),np.max(actreward.reward)))
-    f_ax1.set_ylabel('rewards')
-    f_ax1.set_xlabel('time (msec)')
-    #plot mean weights of all connections onto EML and EMR
-    f_ax2.plot(utimes,Lwts,'r-o',markersize=1)
-    f_ax2.plot(utimes,Rwts,'b-o',markersize=1)
-    f_ax2.set_xlim((0,simConfig['simConfig']['duration']))
-    f_ax2.set_ylim((np.min([np.min(Lwts),np.min(Rwts)]),np.max([np.max(Lwts),np.max(Rwts)])))
-    f_ax2.set_ylabel('total weights')
-    f_ax2.set_xlabel('time (msec)')
-    f_ax2.legend(('->EML','->EMR'),loc='upper left')
-    #f_ax1.legend(('HitBall','FollowBall','ScorePoint'),loc='upper left')
-    f_ax = []
-    for rows in range(3):
-        for cols in range(8):
-            f_ax.append(fig.add_subplot(gs[rows+1,cols]))
-    #fig, axs = plt.subplots(3, 8, figsize=(18,10)); lax = axs.ravel()
-    cbaxes = fig.add_axes([0.95, 0.4, 0.01, 0.2]) 
-    ltitle = ['Excit V1->ML','Excit V1->MR', 'Excit V4->ML','Excit V4->MR', 'Excit MT->ML', 'Excit MT->MR','Excit DirE->ML','Excit DirE->MR','Excit DirNE->ML','Excit DirNE->MR','Excit DirN->ML','Excit DirN->MR','Excit DirNW->ML','Excit DirNW->MR','Excit DirW->ML','Excit DirW->MR','Excit DirSW->ML','Excit DirSW->MR','Excit DirS->ML','Excit DirS->MR','Excit DirSE->ML','Excit DirSE->MR']
-    lfnimage = []
-    tinds = 0
-    maxtstr = len(str(len(utimes)))
-    for t in utimes:
-        f_ax1.plot([t,t],[np.min(actreward.reward),np.max(actreward.reward)],'r',linewidth=0.2)
-        f_ax2.plot([t,t],[np.min([np.min(Lwts),np.min(Rwts)]),np.max([np.max(Lwts),np.max(Rwts)])],'r',linewidth=0.2)
-        pinds = 0
-        tinds = tinds+1
-        fig.suptitle('Connection Weights at Time ' + str(t) + ' ms')
-        for src in ['EV1', 'EV4', 'EMT','EV1DE','EV1DNE','EV1DN','EV1DNW','EV1DW','EV1DSW','EV1DS','EV1DSE']:
-            cpdfL = pdf[(pdf.time==t) & (pdf.postid>=dstartidx['EML']) & (pdf.postid<=dendidx['EML']) & (pdf.preid>=dstartidx[src]) & (pdf.preid<=dendidx[src])]
-            wts1l = np.array(cpdfL.weight)
-            wts2l = np.reshape(wts1l,(int(len(wts1l)/2),2))
-            wtsl = np.sum(wts2l,1)
-            wtsL = np.reshape(wtsl,(int(np.sqrt(len(wtsl))),int(np.sqrt(len(wtsl))))) #assuming neurons in each layer are in square configuration--may need adaptation later
-            cpdfR = pdf[(pdf.time==t) & (pdf.postid>=dstartidx['EMR']) & (pdf.postid<=dendidx['EMR']) & (pdf.preid>=dstartidx[src]) & (pdf.preid<=dendidx[src])]
-            wts1r = np.array(cpdfL.weight)
-            wts2r = np.reshape(wts1r,(int(len(wts1r)/2),2))
-            wtsr = np.sum(wts2r,1)
-            wtsR = np.reshape(wtsr,(int(np.sqrt(len(wtsr))),int(np.sqrt(len(wtsr))))) #assuming neurons in each layer are in square configuration--may need adaptation later
-            ax=f_ax[pinds]
-            pcm = ax.imshow(wtsL, origin='upper', cmap='gray', vmin=minwt, vmax=maxwt+wtrange)
-            ax.set_title(ltitle[pinds])
-            pinds=pinds+1
-            ax=f_ax[pinds]
-            pcm = ax.imshow(wtsR, origin='upper', cmap='gray', vmin=minwt, vmax=maxwt+wtrange)
-            ax.set_title(ltitle[pinds])
-            if pinds==15: plt.colorbar(pcm, cax = cbaxes)
-            pinds = pinds+1
-        f_ax[22].axis('off')
-        f_ax[23].axis('off')
-        if gifpath is not None or mp4path is not None:
-            ctstrl = len(str(tinds))
-            tpre = ''
-            for ttt in range(maxtstr-ctstrl):
-                tpre = tpre+'0'
-            fname = tpre+str(tinds)+'.png'
-            fnimg = '/tmp/'+fname
-            savefig(fnimg); lfnimage.append(fnimg)
-        if pauset>0: plt.pause(pauset)
-    if gifpath is not None: anim.savegif(lfnimage, gifpath)
-    if mp4path is not None: anim.savemp4('/tmp/*.png', mp4path, framerate)
-    for fn in lfnimage: os.unlink(fn) # remove the tmp files
+  utimes = np.unique(pdf.time)
+  maxNMDAwt = np.max(pdf[pdf.syntype=='NMDA']).weight
+  maxAMPAwt = np.max(pdf[pdf.syntype=='AMPA']).weight
+  maxwt = maxNMDAwt+maxAMPAwt
+  minNMDAwt = np.min(pdf[pdf.syntype=='NMDA']).weight
+  minAMPAwt = np.min(pdf[pdf.syntype=='AMPA']).weight
+  minwt = minNMDAwt+minAMPAwt
+  wtrange = 0.1*(maxwt-minwt)
+  fig = plt.figure(figsize=(18,10))
+  gs = fig.add_gridspec(4,8)
+  f_ax1 = fig.add_subplot(gs[0,0:3])
+  f_ax2 = fig.add_subplot(gs[0,4:7])
+  pdfsL = pdf[(pdf.postid>=dstartidx['EML']) & (pdf.postid<=dendidx['EML'])]
+  pdfsR = pdf[(pdf.postid>=dstartidx['EMR']) & (pdf.postid<=dendidx['EMR'])]
+  Lwts = [] #wts of connections onto EML
+  Rwts = [] #wts of connections onto EMR
+  for t in utimes:
+    ct_Lwts = pdfsL[(pdfsL.time==t)].weight
+    ct_Rwts = pdfsR[(pdfsR.time==t)].weight
+    Lwts.append(np.mean(ct_Lwts))
+    Rwts.append(np.mean(ct_Rwts))
+  actionvsproposed = actreward.action-actreward.proposed
+  followtheball = actreward[actionvsproposed==0]
+  f_ax1.plot(actreward.time,actreward.reward,'ko',markersize=2)
+  f_ax1.set_xlim((0,simConfig['simConfig']['duration']))
+  f_ax1.set_ylim((np.min(actreward.reward),np.max(actreward.reward)))
+  f_ax1.set_ylabel('rewards')
+  f_ax1.set_xlabel('time (msec)')
+  #plot mean weights of all connections onto EML and EMR
+  f_ax2.plot(utimes,Lwts,'r-o',markersize=1)
+  f_ax2.plot(utimes,Rwts,'b-o',markersize=1)
+  f_ax2.set_xlim((0,simConfig['simConfig']['duration']))
+  f_ax2.set_ylim((np.min([np.min(Lwts),np.min(Rwts)]),np.max([np.max(Lwts),np.max(Rwts)])))
+  f_ax2.set_ylabel('total weights')
+  f_ax2.set_xlabel('time (msec)')
+  f_ax2.legend(('->EML','->EMR'),loc='upper left')
+  #f_ax1.legend(('HitBall','FollowBall','ScorePoint'),loc='upper left')
+  f_ax = []
+  for rows in range(3):
+    for cols in range(8):
+      f_ax.append(fig.add_subplot(gs[rows+1,cols]))
+  #fig, axs = plt.subplots(3, 8, figsize=(18,10)); lax = axs.ravel()
+  cbaxes = fig.add_axes([0.95, 0.4, 0.01, 0.2]) 
+  ltitle = ['Excit V1->ML','Excit V1->MR', 'Excit V4->ML','Excit V4->MR', 'Excit MT->ML', 'Excit MT->MR','Excit DirE->ML','Excit DirE->MR','Excit DirNE->ML','Excit DirNE->MR','Excit DirN->ML','Excit DirN->MR','Excit DirNW->ML','Excit DirNW->MR','Excit DirW->ML','Excit DirW->MR','Excit DirSW->ML','Excit DirSW->MR','Excit DirS->ML','Excit DirS->MR','Excit DirSE->ML','Excit DirSE->MR']
+  lfnimage = []
+  tinds = 0
+  maxtstr = len(str(len(utimes)))
+  for t in utimes:
+    f_ax1.plot([t,t],[np.min(actreward.reward),np.max(actreward.reward)],'r',linewidth=0.2)
+    f_ax2.plot([t,t],[np.min([np.min(Lwts),np.min(Rwts)]),np.max([np.max(Lwts),np.max(Rwts)])],'r',linewidth=0.2)
+    pinds = 0
+    tinds = tinds+1
+    fig.suptitle('Connection Weights at Time ' + str(t) + ' ms')
+    for src in ['EV1', 'EV4', 'EMT','EV1DE','EV1DNE','EV1DN','EV1DNW','EV1DW','EV1DSW','EV1DS','EV1DSE']:
+      cpdfL = pdf[(pdf.time==t) & (pdf.postid>=dstartidx['EML']) & (pdf.postid<=dendidx['EML']) & (pdf.preid>=dstartidx[src]) & (pdf.preid<=dendidx[src])]
+      wts1l = np.array(cpdfL.weight)
+      wts2l = np.reshape(wts1l,(int(len(wts1l)/2),2))
+      wtsl = np.sum(wts2l,1)
+      wtsL = np.reshape(wtsl,(int(np.sqrt(len(wtsl))),int(np.sqrt(len(wtsl))))) #assuming neurons in each layer are in square configuration--may need adaptation later
+      cpdfR = pdf[(pdf.time==t) & (pdf.postid>=dstartidx['EMR']) & (pdf.postid<=dendidx['EMR']) & (pdf.preid>=dstartidx[src]) & (pdf.preid<=dendidx[src])]
+      wts1r = np.array(cpdfL.weight)
+      wts2r = np.reshape(wts1r,(int(len(wts1r)/2),2))
+      wtsr = np.sum(wts2r,1)
+      wtsR = np.reshape(wtsr,(int(np.sqrt(len(wtsr))),int(np.sqrt(len(wtsr))))) #assuming neurons in each layer are in square configuration--may need adaptation later
+      ax=f_ax[pinds]
+      pcm = ax.imshow(wtsL, origin='upper', cmap='gray', vmin=minwt, vmax=maxwt+wtrange)
+      ax.set_title(ltitle[pinds])
+      pinds=pinds+1
+      ax=f_ax[pinds]
+      pcm = ax.imshow(wtsR, origin='upper', cmap='gray', vmin=minwt, vmax=maxwt+wtrange)
+      ax.set_title(ltitle[pinds])
+      if pinds==15: plt.colorbar(pcm, cax = cbaxes)
+      pinds = pinds+1
+    f_ax[22].axis('off')
+    f_ax[23].axis('off')
+    if gifpath is not None or mp4path is not None:
+      ctstrl = len(str(tinds))
+      tpre = ''
+      for ttt in range(maxtstr-ctstrl):
+        tpre = tpre+'0'
+      fname = tpre+str(tinds)+'.png'
+      fnimg = '/tmp/'+fname
+      savefig(fnimg); lfnimage.append(fnimg)
+    if pauset>0: plt.pause(pauset)
+  if gifpath is not None: anim.savegif(lfnimage, gifpath)
+  if mp4path is not None: anim.savemp4('/tmp/*.png', mp4path, framerate)
+  for fn in lfnimage: os.unlink(fn) # remove the tmp files
 
+#
+def animSynWeights (pdf, gifpath=None, mp4path=None, framerate=10, figsize=None):
+  print('in animSynWeights')
+  ion()
+  utimes = np.unique(pdf.time)
+  maxNMDAwt = np.max(pdf[pdf.syntype=='NMDA']).weight
+  maxAMPAwt = np.max(pdf[pdf.syntype=='AMPA']).weight
+  maxwt = maxNMDAwt+maxAMPAwt
+  minNMDAwt = np.min(pdf[pdf.syntype=='NMDA']).weight
+  minAMPAwt = np.min(pdf[pdf.syntype=='AMPA']).weight
+  minwt = minNMDAwt+minAMPAwt
+  wtrange = 0.1*(maxwt-minwt)
+  if figsize is not None:
+    fig = plt.figure(figsize=figsize)
+  else:
+    fig = plt.figure()
+  gs = fig.add_gridspec(4,8)
+  f_ax1 = fig.add_subplot(gs[0,0:3])
+  f_ax2 = fig.add_subplot(gs[0,4:7])
+  pdfsL = pdf[(pdf.postid>=dstartidx['EML']) & (pdf.postid<=dendidx['EML'])]
+  pdfsR = pdf[(pdf.postid>=dstartidx['EMR']) & (pdf.postid<=dendidx['EMR'])]
+  Lwts = [] #wts of connections onto EML
+  Rwts = [] #wts of connections onto EMR
+  for t in utimes:
+    ct_Lwts = pdfsL[(pdfsL.time==t)].weight
+    ct_Rwts = pdfsR[(pdfsR.time==t)].weight
+    Lwts.append(np.mean(ct_Lwts))
+    Rwts.append(np.mean(ct_Rwts))
+  actionvsproposed = actreward.action-actreward.proposed
+  followtheball = actreward[actionvsproposed==0]
+  f_ax1.plot(actreward.time,actreward.reward,'ko',markersize=2)
+  f_ax1.set_xlim((0,simConfig['simConfig']['duration']))
+  f_ax1.set_ylim((np.min(actreward.reward),np.max(actreward.reward)))
+  f_ax1.set_ylabel('rewards')
+  f_ax1.set_xlabel('time (msec)')
+  #plot mean weights of all connections onto EML and EMR
+  f_ax2.plot(utimes,Lwts,'r-o',markersize=1)
+  f_ax2.plot(utimes,Rwts,'b-o',markersize=1)
+  f_ax2.set_xlim((0,simConfig['simConfig']['duration']))
+  f_ax2.set_ylim((np.min([np.min(Lwts),np.min(Rwts)]),np.max([np.max(Lwts),np.max(Rwts)])))
+  f_ax2.set_ylabel('total weights')
+  f_ax2.set_xlabel('time (msec)')
+  f_ax2.legend(('->EML','->EMR'),loc='upper left')
+  #f_ax1.legend(('HitBall','FollowBall','ScorePoint'),loc='upper left')
+  f_ax = []
+  for rows in range(3):
+    for cols in range(8):
+      f_ax.append(fig.add_subplot(gs[rows+1,cols]))
+  #fig, axs = plt.subplots(3, 8, figsize=(18,10)); lax = axs.ravel()
+  cbaxes = fig.add_axes([0.95, 0.4, 0.01, 0.2]) 
+  ltitle = ['Excit V1->ML','Excit V1->MR', 'Excit V4->ML','Excit V4->MR', 'Excit MT->ML', 'Excit MT->MR','Excit DirE->ML','Excit DirE->MR','Excit DirNE->ML','Excit DirNE->MR','Excit DirN->ML','Excit DirN->MR','Excit DirNW->ML','Excit DirNW->MR','Excit DirW->ML','Excit DirW->MR','Excit DirSW->ML','Excit DirSW->MR','Excit DirS->ML','Excit DirS->MR','Excit DirSE->ML','Excit DirSE->MR']
+  f_ax[22].axis('off')
+  f_ax[23].axis('off')
+  lsrc = ['EV1', 'EV4', 'EMT','EV1DE','EV1DNE','EV1DN','EV1DNW','EV1DW','EV1DSW','EV1DS','EV1DSE']
+  dimg = {}
+  for src in lsrc:
+    t = utimes[0]
+    f_ax1.plot([t,t],[np.min(actreward.reward),np.max(actreward.reward)],'r',linewidth=0.2)
+    f_ax2.plot([t,t],[np.min([np.min(Lwts),np.min(Rwts)]),np.max([np.max(Lwts),np.max(Rwts)])],'r',linewidth=0.2)
+    pinds = 0
+    fig.suptitle('t=' + str(t) + ' ms')
+    for src in lsrc:
+      cpdfL = pdf[(pdf.time==t) & (pdf.postid>=dstartidx['EML']) & (pdf.postid<=dendidx['EML']) & (pdf.preid>=dstartidx[src]) & (pdf.preid<=dendidx[src])]
+      wts1l = np.array(cpdfL.weight)
+      wts2l = np.reshape(wts1l,(int(len(wts1l)/2),2))
+      wtsl = np.sum(wts2l,1)
+      #assuming neurons in each layer are in square configuration--may need adaptation later
+      wtsL = np.reshape(wtsl,(int(np.sqrt(len(wtsl))),int(np.sqrt(len(wtsl))))) 
+      cpdfR = pdf[(pdf.time==t) & (pdf.postid>=dstartidx['EMR']) & (pdf.postid<=dendidx['EMR']) & (pdf.preid>=dstartidx[src]) & (pdf.preid<=dendidx[src])]
+      wts1r = np.array(cpdfL.weight)
+      wts2r = np.reshape(wts1r,(int(len(wts1r)/2),2))
+      wtsr = np.sum(wts2r,1)
+      #assuming neurons in each layer are in square configuration--may need adaptation later
+      wtsR = np.reshape(wtsr,(int(np.sqrt(len(wtsr))),int(np.sqrt(len(wtsr))))) 
+      ax=f_ax[pinds]
+      dimg[pinds] = pcm = ax.imshow(wtsL, origin='upper', cmap='gray', vmin=minwt, vmax=maxwt+wtrange)
+      ax.set_title(ltitle[pinds])
+      pinds=pinds+1
+      ax=f_ax[pinds]
+      dimg[pinds] = pcm = ax.imshow(wtsR, origin='upper', cmap='gray', vmin=minwt, vmax=maxwt+wtrange)
+      ax.set_title(ltitle[pinds])
+      if pinds==15: plt.colorbar(pcm, cax = cbaxes)
+      pinds = pinds+1              
+  def updatefig (tdx):
+    t = utimes[tdx]
+    f_ax1.plot([t,t],[np.min(actreward.reward),np.max(actreward.reward)],'r',linewidth=0.2)
+    f_ax2.plot([t,t],[np.min([np.min(Lwts),np.min(Rwts)]),np.max([np.max(Lwts),np.max(Rwts)])],'r',linewidth=0.2)
+    pinds = 0
+    fig.suptitle('t=' + str(t) + ' ms')
+    for src in lsrc:
+      cpdfL = pdf[(pdf.time==t) & (pdf.postid>=dstartidx['EML']) & (pdf.postid<=dendidx['EML']) & (pdf.preid>=dstartidx[src]) & (pdf.preid<=dendidx[src])]
+      wts1l = np.array(cpdfL.weight)
+      wts2l = np.reshape(wts1l,(int(len(wts1l)/2),2))
+      wtsl = np.sum(wts2l,1)
+      #assuming neurons in each layer are in square configuration--may need adaptation later
+      wtsL = np.reshape(wtsl,(int(np.sqrt(len(wtsl))),int(np.sqrt(len(wtsl))))) 
+      cpdfR = pdf[(pdf.time==t) & (pdf.postid>=dstartidx['EMR']) & (pdf.postid<=dendidx['EMR']) & (pdf.preid>=dstartidx[src]) & (pdf.preid<=dendidx[src])]
+      wts1r = np.array(cpdfL.weight)
+      wts2r = np.reshape(wts1r,(int(len(wts1r)/2),2))
+      wtsr = np.sum(wts2r,1)
+      #assuming neurons in each layer are in square configuration--may need adaptation later
+      wtsR = np.reshape(wtsr,(int(np.sqrt(len(wtsr))),int(np.sqrt(len(wtsr))))) 
+      dimg[pinds].set_data(wtsL)
+      pinds=pinds+1
+      dimg[pinds].set_data(wtsR)
+      pinds = pinds+1      
+    return fig
+  ani = animation.FuncAnimation(fig, updatefig, interval=1, frames=len(utimes))
+  if mp4path is not None:
+    # Either avconv or ffmpeg need to be installed in the system to produce the videos!
+    try:
+      writer = animation.writers['ffmpeg']
+    except KeyError:
+      writer = animation.writers['avconv']
+    writer = writer(fps=framerate)
+    ani.save(mp4path, writer=writer)
+  if gifpath is not None:
+    try:
+      writer = animation.writers['imagemagick'] # need to have imagemagick installed
+      writer = writer(fps=framerate)
+      ani.save(gifpath, writer=writer)
+    except:
+      print('imagemagick not available')
+    
         
 
 
@@ -390,8 +510,9 @@ if __name__ == '__main__':
     stepNB = int(sys.argv[1]) #which file(stepNB) want to plot
   print(stepNB)
   simConfig, pdf, actreward, dstartidx, dendidx = loadsimdat()
+  print('loaded simulation data')
   #davgw = plotavgweights(pdf)
-  plotSynWeightsPerTimeStep(pdf,pauset=1,mp4path='data/'+dconf['sim']['name']+'weightmap.mp4', framerate=10) #plot images
+  animSynWeights(pdf,mp4path='data/'+dconf['sim']['name']+'weightmap.mp4', framerate=10) #plot images
   #wperPostID = plotavgweightsPerPostSynNeuron1(pdf)
   #plotavgweightsPerPostSynNeuron2(pdf)
   #plotIndividualSynWeights(pdf)
