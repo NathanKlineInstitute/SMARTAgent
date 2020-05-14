276bf1aa5ad38ec6b0a0a67e2b0367ac5c385462
Sam Neymotin <samnemo@gmail.com>
2020-05-14 09:46:28 -0400

    print out json name when not default; looking at output from last few sims

--- conf.py
+++ conf.py
@@ -6,6 +6,7 @@ fnjson = 'sim.json'
 for i in range(len(sys.argv)):
   if sys.argv[i].endswith('.json'):
     fnjson = sys.argv[i]
+    print('reading ', fnjson)
 
 with open(fnjson,'r') as fp:
   dconf = json.load(fp)
